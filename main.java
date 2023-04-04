4be3158722b0382c0aba5827d47d8eb417e45e44
25sudharsan27 <25sudharsan27@gmail>
2023-04-04 23:15:34 +0530

    first command

---
+++ main.java
@@ -0,0 +1,27 @@
+package com.sudharsan;
+
+public class main {
+     public static void main(String[] args){
+         // our first program
+
+//         System.out.println("Hello Java");
+//         String name = "Sudharsan";
+//         int age = 19 ;
+//         String neighbouor="Rohan";
+//         String friend ="Nithish";
+         //types
+         //byte -1 [-128 to 127]
+         //short - 2
+         //int - 4  1,2,3
+         //long - 8
+         //float - 4 3.14
+         //double - 8
+         //char - 2 a,b,c,d
+         //boolean - 1
+//         byte age = 30;
+//         int phone = 123456789;
+         String name="Sudharsan";
+         System.out.println(name.length());
+
+     }
+}
