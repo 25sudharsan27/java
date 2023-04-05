570c622c91f3d137478843c68430d6e31097d4b0
Sudharsan B <117500265+25sudharsan27@users.noreply.github.com>
2023-04-05 19:37:30 +0530

    how to handle strings

---
+++ string.java
@@ -0,0 +1,12 @@
+class string{
+    public static void main (String[] args)
+    {
+        String name1 ="Aman";
+        String name2 ="Akku";
+        String name3=name1+" and "+name2;
+        System.out.println(name3);
+        System.out.println(name1.charAt(0));
+        String name4= name1.replace('m','s');
+        System.out.println(name4);
+    }
+}
